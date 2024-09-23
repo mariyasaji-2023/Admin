49f84ef6a9f712ce876fbc2d7da8fc35de65cb44
mariyasaji-2023 <mariyasajichiramel.com>
2024-09-23 16:30:35 +0530

    fourth commit

--- server.js
+++ server.js
@@ -10,5 +10,9 @@ connectDB()
 
 app.use(express.json())
 app.use('/api/auth', authRoutes)
+app.get('/', (req, res) => {
+    res.send('Welcome to the Admin Login API');
+});
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
