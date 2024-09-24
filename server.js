16af9fe1182ce482c34fdb84e754b47e8f0aa235
mariyasaji-2023 <mariyasajichiramel.com>
2024-09-24 13:25:54 +0530

    cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const connectDB = require('./src/config/database')
 const authRoutes = require('./src/routes/authRoutes')
 require('dotenv').config()
+const cors = require('cors'); 
 
 const app = express()
 console.log("Mongo URI:", process.env.MONGO_URI); // Debugging
 
 connectDB()
-
+app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.get('/', (req, res) => {
